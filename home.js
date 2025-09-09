f7783e16127eb1fdb56651601c4b11acbf24725a
tanim542 <tshahrear0@gmail.com>
2025-09-10 03:50:51 +0600

    some js added in home page

--- home.js
+++ home.js
@@ -1,3 +1,5 @@
+const validPin = 1234;
+const validAccountNumber = "1894142714";
 document.getElementById("btn-add-money")
     .addEventListener("click", function (event) {
         event.preventDefault();
@@ -5,10 +7,21 @@ document.getElementById("btn-add-money")
         const bank = document.getElementById("bank").value;
         const accountNumber = document.getElementById("account-number").value;
         const addAmount = parseInt(document.getElementById("add-amount").value);
-        const addPin = document.getElementById("add-pin").value;
+        const addPin = parseInt(document.getElementById("add-pin").value);
 
         const availableBalance = parseInt(document.getElementById("available-balance").innerText);
-
+        if(accountNumber.length !== 10){
+            alert("Please enter a valid account number");
+            return;
+        }
+        if(validAccountNumber !== accountNumber){
+            alert("Invalid account number");
+            return;
+        }
+        if(addPin !== validPin){
+            alert("Invalid pin");
+            return;
+        }
         const totalBalance = availableBalance + addAmount;
 
         document.getElementById("available-balance").innerText = totalBalance;
