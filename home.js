ba28d597050bcf95c99608a186e8c6ae76d81d2d
tanim542 <tshahrear0@gmail.com>
2025-09-25 02:22:54 +0600

    added some reuseable functions

--- home.js
+++ home.js
@@ -1,19 +1,63 @@
+// function to get input value 
+function getInputValue(id){
+    const inputField = document.getElementById(id);
+    const inputFieldValue = inputField.value;
+    const inputValueConverted = parseInt(inputFieldValue);
+    return inputValueConverted;
+}
+
+// function to get inner text value
+function getInnerText(id){
+   const value = parseInt(document.getElementById(id).innerText); 
+   return value;
+}
+
+// function to set innerText
+function setInnerText (value){
+    document.getElementById("available-balance").innerText = value;
+}
+
+// function to toggle features
+function handleToggle(id){
+    const forms = document.getElementsByClassName("form");
+
+    for(const form of forms){
+        form.style.display = "none";
+    }
+    document.getElementById(id).style.display = "block";
+
+}
+
+// function to toggle button 
+function handleButtonToggle(id){
+    const formBtns = document.getElementsByClassName("form-btn");
+    
+    for(const btn of formBtns){
+        btn.classList.remove("border-[#0874f2]", "bg-[#f2f8fe]");
+        btn.classList.add("border-[#08080880]");
+    }
+
+    document.getElementById(id).classList.add("border-[#0874f2]", "bg-[#f2f8fe]");
+    document.getElementById(id).classList.remove("border-[#08080880]");
+}
+
+
 const validPin = 1234;
-const validAccountNumber = "1894142714";
+const validAccountNumber = 1894142714;
+
 document.getElementById("btn-add-money")
     .addEventListener("click", function (event) {
         event.preventDefault();
 
-        const bank = document.getElementById("bank").value;
-        const accountNumber = document.getElementById("account-number").value;
-        const addAmount = parseInt(document.getElementById("add-amount").value);
-        const addPin = parseInt(document.getElementById("add-pin").value);
+        const bank = getInputValue("bank");
+        const accountNumber = getInputValue("account-number");
+        const addAmount = getInputValue("add-amount");
+        const addPin = getInputValue("add-pin");
 
-        const availableBalance = parseInt(document.getElementById("available-balance").innerText);
-        if(accountNumber.length !== 10){
-            alert("Please enter a valid account number");
-            return;
-        }
+        const availableBalance = getInnerText("available-balance");
+
+        
+
         if(validAccountNumber !== accountNumber){
             alert("Invalid account number");
             return;
@@ -24,7 +68,7 @@ document.getElementById("btn-add-money")
         }
         const totalBalance = availableBalance + addAmount;
 
-        document.getElementById("available-balance").innerText = totalBalance;
+        setInnerText(totalBalance);
 
         document.getElementById("add-amount").value = '';
 
@@ -34,22 +78,23 @@ document.getElementById("btn-add-money")
     // cash out section 
     document.getElementById("btn-withdraw-money").addEventListener("click", function(event){
         event.preventDefault();
-        const amount = parseInt(document.getElementById("withdraw-amount").value);
+        const amount = getInputValue("withdraw-amount");
 
-        const availableBalance = parseInt(document.getElementById("available-balance").innerText);
+        const availableBalance = getInnerText("available-balance");
 
         const totalAvailableBalance = availableBalance - amount;
 
-        document.getElementById("available-balance").innerText = totalAvailableBalance;
+        setInnerText(totalAvailableBalance);
+
         document.getElementById("withdraw-amount").value = '';
     })
 
     // toggle features 
     document.getElementById("add-money-button").addEventListener("click", function(){
-        document.getElementById("cashout-parent").style.display = "none";
-        document.getElementById("add-money-parent").style.display = "block";   
+           handleToggle("add-money-parent");
+           handleButtonToggle("add-money-button");
     })
     document.getElementById("cashout-button").addEventListener("click", function(){
-        document.getElementById("add-money-parent").style.display = "none";
-        document.getElementById("cashout-parent").style.display = "block";   
+         handleToggle("cashout-parent");
+         handleButtonToggle("cashout-button");
     })
